06ceb926401d967eff11b3b67f39d1d1173b47d1
AndresMpa <andresprieto2703@gmail.com>
2021-05-09 04:56:43 -0500

    He agregado un web scrapper de imagenes

---
+++ Software/web_scrapping/images_scrapping/index.py
@@ -0,0 +1,111 @@
+# Basic
+from random import choice
+from io import BytesIO
+from os import path as os_path
+# Request
+from requests import get
+# Pillow
+from PIL import Image
+# Selenim
+from selenium.webdriver import Firefox
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as ecs
+
+def create_scrapper(driver):
+    # Opciones para para la instancia del navegador
+    options = Options()
+    options.headless = True
+    # Implementacion del servicio de proxy
+    PROXY = '127.63.13.19:3184'
+    options.add_argument('--proxy-server=%s' % PROXY)
+
+    # Instancia del navigador
+    new_scrapper = driver(options=options)
+
+    return new_scrapper
+
+def scroll(scrapper):
+    scroll_page_down = 'window.scrollTo(0, document.body.scrollHeight);'
+    scrapper.execute_script(scroll_page_down)
+    wait(scrapper, 20)
+
+def get_urls(images, scrapper):
+    # Clickamos cada imagen y guardamos su url
+    images_urls = set()
+    for img in images[0:len(images)]:
+        try:
+            img.click()
+            wait(scrapper, 20)
+        except Exception:
+            continue
+
+        # Sacamos lo links de las imagenes
+        current_img = scrapper.find_element(By.CSS_SELECTOR, 'img.n3VNCb')
+        if 'http' in current_img.get_attribute('src'):
+            images_urls.add(current_img.get_attribute('src'))
+
+    return images_urls
+
+def fetch_query(query:str, number_of_images:int, query_scrapper):
+    # Definimos un proveedor de imagenes
+    google_images = "https://www.google.com/search?safe=off&site=&tbm=isch&source=hp&q={q}&oq={q}&gs_l=img"
+    query_scrapper.get(google_images.format(q=query))
+
+    # Bajamos hasta el final del domento
+    scroll(query_scrapper)
+
+    # Recogemos las urls
+    res = query_scrapper.find_elements(By.CSS_SELECTOR, 'img.Q4LuWd')
+    wait(scrapper, 20)
+
+    # Capturamos las URL de cada imagen
+    urls = get_urls(res, query_scrapper)
+
+    # Mostramos la cantidad de imagenes
+    print("Se han encontractrado", len(urls), " imagenes que coinciden con tu patrón de busqueda, tomaremos solo ", number_of_images ,end='\n')
+
+    # De los links sacamos solo la cantidad requerida
+    images = []
+    while len(images) <= number_of_images:
+        choosen = choice(list(urls))
+        if choosen not in images:
+            images.append(choosen)
+
+    return images
+
+def download_res(urls:str, path:str):
+    cont = 0
+    for url in urls:
+        try:
+            # Se optiene la imagen
+            img_content = get(url).content
+        except Exception as e:
+            print('No fue posible descargar la imagen ', str(url) + ' ', end='')
+            print(e)
+
+        try:
+            # Se genera el archivo de la imagen
+            image_file = BytesIO(img_content)
+            image = Image.open(image_file).convert('RGB')
+            file_path = os_path.join(path + '/imagen_' + str(cont) + '.jpg')
+
+            # Se guarda la imagen
+            with open(file_path, 'wb') as f:
+                image.save(f, "JPEG", quality=85)
+
+            # Se confirma que se guardo la imagen
+            print('Guardado exitosamente -' + str(url) + '- en ' + file_path)
+        except Exception as e:
+            print('ERROR - no se pudo guardar la imagen ' + str(url))
+            print(e)
+        cont += 1
+
+if __name__ == '__main__':
+    # Generamos un scrapper
+    scrapper = create_scrapper(Firefox)
+    res_urls = fetch_query('conejitos lindos', 10, scrapper)
+    download_res(res_urls, '/home/andresmp/Escritorio/Scrapper')
+
